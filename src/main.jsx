482a4836f3b89884c43dd371e84c35c48d2365ba
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-11-25 01:22:11 +0530

    Basic chanllenge for React
    Set up a new React app form scratch!

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/**
+ * Challenge : Set up a new React app from scratch!
+ * Hints:
+ * 1. Need to import the method to create a nwe root
+ * from the "react-dom/client" package
+ * 2. Use that root to render some JSX. For now just render
+ * an h1 element that says anything you want.
+ */
+
+import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"))
 
-/*
-
-Challenge - recreate previous commit using vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
-
-- Create a new h1 element (createElement)
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child (using 'appendChild') of the div#root
-
-Don't use innerHTML to accomplish any of this.
-
-*/
-
-const h1_element = document.createElement("h1");
-h1_element.textContent = "This is a manual imperative coding"
-h1_element.class = "header"
-document.getElementById("root").appendChild(h1_element)
-
 root.render(
-  h1_element
+  <h1>
+    Dheeraj completed the Challenge!
+  </h1>
 )
+
