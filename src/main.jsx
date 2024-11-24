c2ae6cbe0d116d9a6c5c0334ab2389bac37ee6a4
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-11-25 00:27:32 +0530

    React:Declarative vs Imperative

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,25 @@ import App from './App.jsx'
 
 const root = createRoot(document.getElementById("root"))
 
-function MainContent() {
-  return <h1>React is Great!</h1>
-}
+/*
+
+Challenge - recreate previous commit using vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element (createElement)
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child (using 'appendChild') of the div#root
+
+Don't use innerHTML to accomplish any of this.
+
+*/
+
+const h1_element = document.createElement("h1");
+h1_element.textContent = "This is a manual imperative coding"
+h1_element.class = "header"
+document.getElementById("root").appendChild(h1_element)
 
 root.render(
-  <div>
-    <MainContent />
-  </div>
+  h1_element
 )
