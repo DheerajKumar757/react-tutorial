9dfa056542a8c6139d05ac2261baf1455fb7654f
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-11-25 01:24:17 +0530

    Fully retain the default React App Website.
    This commit is like a fresh install of a React App.
    If we checkout to this commit we get fresh
    Default React Project.

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
-/**
- * Challenge : Set up a new React app from scratch!
- * Hints:
- * 1. Need to import the method to create a nwe root
- * from the "react-dom/client" package
- * 2. Use that root to render some JSX. For now just render
- * an h1 element that says anything you want.
- */
-
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.getElementById("root"))
-
-root.render(
-  <h1>
-    Dheeraj completed the Challenge!
-  </h1>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
