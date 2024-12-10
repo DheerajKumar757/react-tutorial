7ec4791ce239aaf2eb89c542d483916f5e2ed670
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-12-10 18:57:04 +0530

    Create custom components and different ways to use it.

--- src/tutorial.jsx
+++ src/tutorial.jsx
@@ -12,14 +12,23 @@ import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"))
 root.render(
     <main>
-        <img src="/src/assets/react-logo.png" width="40px" alt="React logo"/>
-        <h1>Fun facts about React!</h1>
-        <ul>
-            <li>Was first release in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 200K stars on Github</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <TemporaryName></TemporaryName>
+        <TemporaryName />
     </main>
 )
+
+function TemporaryName() {
+    return (
+        <main>
+            <img src="/src/assets/react-logo.png" width="40px" alt="React logo"/>
+            <h1>Fun facts about React!</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
