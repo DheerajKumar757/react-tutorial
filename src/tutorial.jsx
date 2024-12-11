9dba1658062347fa81c8259b992768a5ff0cf84a
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-12-11 22:29:41 +0530

    Using Fragments Break Header, Footer and MainContent into different components

--- src/tutorial.jsx
+++ src/tutorial.jsx
@@ -8,35 +8,52 @@
  */
 
 import { createRoot } from "react-dom/client";
+import { Fragment } from "react";
 
 const root = createRoot(document.getElementById("root"))
 root.render(
     <main>
-        <TemporaryName />
+        <Page />
     </main>
 )
 
-function TemporaryName() {
+function Header() {
     return (
-        <div>
-            <header>
-                <img src="/src/assets/react-logo.png" width="40px" alt="React logo"/>
-            </header>
-            
-            <main>
-                <h1>Fun facts about React!</h1>
-                <ul>
-                    <li>Was first release in 2013</li>
-                    <li>Was originally created by Jordan Walke</li>
-                    <li>Has well over 200K stars on Github</li>
-                    <li>Is maintained by Meta</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
-            </main>
+        <header>
+            <img src="/src/assets/react-logo.png" width="40px" alt="React logo"/>
+        </header>
+    )
+}
 
-            <footer>
-                <small>@ 2024 kumar development. All rights reserved. </small>
-            </footer>
-        </div>
+function MainContent() {
+    return (
+        <>
+            <h1>Fun facts about React!</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>@ 2024 kumar development. All rights reserved. </small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <Fragment>
+            <Header />
+            <MainContent />
+            <Footer />
+        </Fragment>
     )
 }
