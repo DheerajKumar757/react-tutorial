11c571d36a2acbcc76685d8b5f0f1d117e13de7e
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2024-12-11 21:56:08 +0530

    Break Code in Header in Footer in a single element.

--- src/tutorial.jsx
+++ src/tutorial.jsx
@@ -12,23 +12,31 @@ import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"))
 root.render(
     <main>
-        <TemporaryName></TemporaryName>
         <TemporaryName />
     </main>
 )
 
 function TemporaryName() {
     return (
-        <main>
-            <img src="/src/assets/react-logo.png" width="40px" alt="React logo"/>
-            <h1>Fun facts about React!</h1>
-            <ul>
-                <li>Was first release in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 200K stars on Github</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </main>
+        <div>
+            <header>
+                <img src="/src/assets/react-logo.png" width="40px" alt="React logo"/>
+            </header>
+            
+            <main>
+                <h1>Fun facts about React!</h1>
+                <ul>
+                    <li>Was first release in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 200K stars on Github</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </main>
+
+            <footer>
+                <small>@ 2024 kumar development. All rights reserved. </small>
+            </footer>
+        </div>
     )
 }
