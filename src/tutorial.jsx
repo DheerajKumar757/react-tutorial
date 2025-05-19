df137d4a626300b1f769c83a022c0b03c3ccf72d
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2025-05-19 21:37:19 +0530

    Header is improved

--- src/tutorial.jsx
+++ src/tutorial.jsx
@@ -1,10 +1,9 @@
 /**
  * Challenge: set up a new React app from scratch!
  * Hints:
- * 1. Need to import the method to create a new root
- *    from the "react-dom/client" package.
- * 2. Use that root to render some JSX. For now, just
- *    render an h1 element.
+ * - Add a 'nav' > 'ul' > 'li' (x3). The items should say:
+ * "Pricing", "About" and "Contact".
+ * 
  */
 
 import { createRoot } from "react-dom/client";
@@ -21,6 +20,14 @@ function Header() {
     return (
         <header>
             <img src="/src/assets/react-logo.png" width="40px" alt="React logo"/>
+            <nav>
+                <ul className="nav-list">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            
         </header>
     )
 }
