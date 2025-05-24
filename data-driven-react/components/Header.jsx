a39ca06a9b2bfd30c7f2bd5fcb8915815dae12dd
Dheeraj Kumar <dheerajkumar12757@gmail.com>
2025-05-24 16:51:34 +0530

    Tavel Journal : Basic project setup and header.

---
+++ data-driven-react/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+        <header>
+            <img src="../images/globe.png" alt="globe icon"/>
+            <h1>My travel journal</h1>
+        </header>
+    )
+}
